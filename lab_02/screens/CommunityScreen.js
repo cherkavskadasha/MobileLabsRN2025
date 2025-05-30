4f8c4de0c07c742cf2f74c9dc719e4a108b4bc7d
Черкавська Дарина <vt232_chdv@student.ztu.edu.ua>
2025-05-30 22:45:18 +0300

    feat: edit CommunityScreen.js

--- lab_02/screens/CommunityScreen.js
+++ lab_02/screens/CommunityScreen.js
@@ -28,31 +28,59 @@ const NewsContent = styled.Text`
   margin-top: 8px;
 `;
 
+const TabsContainer = styled.View`
+    flex-direction: row;
+    margin-bottom: 20px;
+`;
+
+const TabButton = styled.TouchableOpacity`
+  background-color: ${({ active, theme }) =>
+    active ? '#2e8eff' : theme.card};
+  padding: 8px 14px;
+  border-radius: 14px;
+  margin-right: 10px;
+`;
+
+const TabText = styled.Text`
+  color: ${({ active }) => (active ? 'white' : '#aaa')};
+  font-weight: bold;
+`;
+
+const NewsImage = styled.Image`
+  width: 285px;
+  height: 200px;
+`;
+
 const mockNews = [
     {
         id: '1',
         title: 'Steam Summer Sale is coming!',
         content: 'Get ready for massive discounts across thousands of games.',
+        image: require('../assets/KingomCome.png'),
     },
     {
         id: '2',
         title: 'New Update for Cyberpunk 2077',
         content: 'Patch 2.1 brings major improvements and new content.',
+        image: require('../assets/KingomCome.png'),
     },
     {
         id: '3',
         title: 'Free Weekend: Rainbow Six Siege',
         content: 'Play for free this weekend and grab it at 70% off.',
+        image: require('../assets/KingomCome.png'),
     },
 ];
 
+
 export default function CommunityScreen() {
     const [news, setNews] = useState(mockNews);
-    const theme = useTheme();
+    const [activeTab, setActiveTab] = useState('Search');
     const loadMoreNews = () => {
         const newItem = {
             id: Math.random().toString(),
             title: 'New Game Release',
+            image: require('../assets/KingomCome.png'),
             content: 'Check out the latest indie titles added this week.',
         };
         setNews((prev) => [...prev, newItem]);
@@ -61,12 +89,24 @@ export default function CommunityScreen() {
     return (
         <Container>
             <Header title="Community" />
+            <TabsContainer>
+                {['Search', 'All', 'Screenshots', 'Artwork'].map((tab) => (
+                    <TabButton
+                        key={tab}
+                        active={tab === activeTab}
+                        onPress={() => setActiveTab(tab)}
+                    >
+                        <TabText active={tab === activeTab}>{tab}</TabText>
+                    </TabButton>
+                ))}
+            </TabsContainer>
             <FlatList
                 data={news}
                 keyExtractor={(item) => item.id}
                 renderItem={({ item }) => (
                     <NewsCard>
                         <NewsTitle>{item.title}</NewsTitle>
+                        <NewsImage source={item.image} />
                         <NewsContent>{item.content}</NewsContent>
                     </NewsCard>
                 )}
